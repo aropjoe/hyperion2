9f8e6d3239f3fb257b4035b98d07b6bccc5abc3b
Joe Arop <59123451+aropjoe@users.noreply.github.com>
2023-10-17 16:53:15 +0000

    write new module for schemaless data

--- core/utils/schemaless.py
+++ core/utils/schemaless.py
@@ -1,45 +1,102 @@
 import pandas as pd
 import json
+from datetime import datetime
 
-def extract_price_and_sales_data(csv_file):
-    # Read the CSV file into a pandas DataFrame
-    df = pd.read_csv(csv_file)
 
-    # Initialize variables to store the extracted data
-    extracted_data = []
+def process_date(date_string):
+    try:
+        date_object = datetime.strptime(date_string, "%Y-%m-%dT%H:%M:%S.%fZ")
+        formatted_date_string = date_object.strftime("%Y-%m-%d %H:%M:%S")
+        return formatted_date_string
+    except Exception as e:
+        return date_string
 
-    # Iterate through the columns and rows to identify "price" and "sales"
-    for _, row in df.iterrows():
-        price = None
-        sales = None
 
-        for column in df.columns:
-            cell_value = row[column]
+def group_sales(data_frame):
+    #data_frame = data_frame.dropna()
+    data_frame["OrderDate"] = data_frame["OrderDate"].apply(process_date)
+    data_frame['Sales'] = data_frame['Sales'].astype(int)
+    grouped_data = data_frame.groupby("OrderDate")["Sales"].sum().reset_index()
+    data = {
+        "dates": grouped_data["OrderDate"].to_list(),
+        "sales": grouped_data["Sales"].to_list(),
+    }
+    return data
 
-            # Check if the cell contains a numeric value (you can customize this condition)
-            if pd.notna(cell_value) and isinstance(cell_value, (int, float)):
-                if price is None:
-                    price = cell_value
-                elif sales is None:
-                    sales = cell_value
-                else:
-                    break  # Exit the loop if both price and sales are found
 
-        # Append the extracted data to the list
-        if price is not None and sales is not None:
-            extracted_data.append({
-                "price": price,
-                "sales": sales,
-                "timestamp": row.get("timestamp", None)
-            })
+def group_profit(data_frame):
+    #data_frame = data_frame.dropna()
+    data_frame["OrderDate"] = data_frame["OrderDate"].apply(process_date)
+    data_frame['Profit'] = data_frame['Profit'].astype(int)
+    grouped_data = data_frame.groupby("OrderDate")["Profit"].sum().reset_index()
+    data = {
+        "dates": grouped_data["OrderDate"].to_list(),
+        "profit": grouped_data["Profit"].to_list(),
+    }
+    return data
 
-    # Convert the extracted data to JSON
-    json_data = json.dumps(extracted_data)
 
-    return json_data
+def group_segment(data_frame):
+    #data_frame = data_frame.dropna()
+    data_frame['Sales'] = data_frame['Sales'].astype(int)
+    grouped_data = data_frame.groupby("Segment")["Sales"].sum().reset_index()
+    data = {
+        "segment": grouped_data["Segment"].to_list(),
+        "sales": grouped_data["Sales"].to_list(),
+    }
+    return data
 
-# Example usage
-csv_file_path = "your_csv_file.csv"  # Replace with the actual file path
-json_data = extract_price_and_sales_data(csv_file_path)
 
-# You can store or visualize the json_data as needed
+def group_cities(data_frame):
+    #data_frame = data_frame.dropna()
+    data_frame['Sales'] = data_frame['Sales'].astype(int)
+    grouped_data = data_frame.groupby("City")["Sales"].sum().reset_index()
+    data = {
+        "cities": grouped_data["City"].to_list(),
+        "sales": grouped_data["Sales"].to_list(),
+    }
+    return data
+
+
+def group_category(data_frame):
+    #data_frame = data_frame.dropna()
+    data_frame['Sales'] = data_frame['Sales'].astype(int)
+    grouped_data = data_frame.groupby("Category")["Sales"].sum().reset_index()
+    data = {
+        "categories": grouped_data["Category"].to_list(),
+        "sales": grouped_data["Sales"].to_list(),
+    }
+    return data
+
+
+def group_sub_category(data_frame):
+    #data_frame = data_frame.dropna()
+    data_frame['Sales'] = data_frame['Sales'].astype(int)
+    grouped_data = data_frame.groupby("Sub_Category")["Sales"].sum().reset_index()
+    data = {
+        "sub_categories": grouped_data["Sub_Category"].to_list(),
+        "sales": grouped_data["Sales"].to_list(),
+    }
+    return data
+
+
+def group_country(data_frame):
+    #data_frame = data_frame.dropna()
+    data_frame['Sales'] = data_frame['Sales'].astype(int)
+    grouped_data = data_frame.groupby("Country")["Sales"].sum().reset_index()
+    data = {
+        "countries": grouped_data["Country"].to_list(),
+        "sales": grouped_data["Sales"].to_list(),
+    }
+    return data
+
+
+def group_product(data_frame):
+    #data_frame = data_frame.dropna()
+    data_frame['Sales'] = data_frame['Sales'].astype(int)
+    grouped_data = data_frame.groupby("ProductName")["Sales"].sum().reset_index()
+    data = {
+        "products": grouped_data["ProductName"].to_list(),
+        "sales": grouped_data["Sales"].to_list(),
+    }
+    return data
